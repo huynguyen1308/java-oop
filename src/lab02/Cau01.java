8590e749f7f8d37d955492848814a1d8e795329b
huynq <huynq@macs-MacBook-Air.local>
2022-03-02 16:59:06 +0700

    Hoan thanh cau 1 (lab02)

--- src/lab02/Cau01.java
+++ src/lab02/Cau01.java
@@ -1,4 +1,30 @@
 package lab02;
 
+import java.util.Scanner;
+
 public class Cau01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please input your height in meter: ");
+        float height = scanner.nextFloat();
+        System.out.println("Please input your weight in kilogram: ");
+        float weight = scanner.nextFloat();
+        float BMI = weight/(height * 2);
+        System.out.println("Your BMI is:" + BMI);
+
+        if (BMI >= 30) {
+            System.out.println("You are obesity");
+        } else if (BMI >= 25 && BMI <= 29.9) {
+            System.out.println("You are overweight");
+        } else if (BMI > 18.5 && BMI <= 24.9) {
+            System.out.println("You're normal weight!");
+        } else {
+            System.out.println("You are underweight");
+        }
+    }
+
+
+
+
+
 }
