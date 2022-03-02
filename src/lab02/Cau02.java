1d0d119ad48e16d7b8e6fe61406b50f1ed515dd2
huynq <huynq@macs-MacBook-Air.local>
2022-03-02 17:17:33 +0700

    Completed cau 2 (lab02)

---
+++ src/lab02/Cau02.java
@@ -0,0 +1,16 @@
+package lab02;
+
+import java.util.Scanner;
+
+public class Cau02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please input a number!");
+        int number = scanner.nextInt();
+        if (number % 2 == 0) {
+            System.out.println("It is even number");
+        } else {
+            System.out.println("It is an odd number");
+        }
+    }
+}
